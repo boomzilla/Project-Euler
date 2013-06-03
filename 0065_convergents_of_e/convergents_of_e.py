11c5a8219b3f56fc156cb9bf5eb9887a18f69d94
Ian Ruotsala <ian.ruotsala@gmail.com>
2013-06-03 15:31:35 -0700

    description of problem

---
+++ 0065_convergents_of_e/convergents_of_e.py
@@ -0,0 +1,14 @@
+#my solution to Project Euler challenge 65
+#http://projecteuler.net/problem=65
+#
+#number e can be expressed as infinite continued fraction (http://en.wikipedia.org/wiki/Continued_fraction):
+#e = [2; 1,2,1, 1,4,1, 1,6,1 , ... , 1,2k,1, ...]
+#so, for k = 0, e ~= 2
+#k = 1, e ~= 3
+#k = 2, e ~= 8/3
+#k = 3, e ~= 11/4
+#and so on
+#
+#find the numerator of 100th convergent, and sum up those digits
+#
+#Ian Ruotsala, 2013, June, 3rd
