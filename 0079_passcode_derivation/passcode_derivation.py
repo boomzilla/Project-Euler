f4b1e19516dae2dc2b05325e4cd3bcf55133c3ff
Ian Ruotsala <ian.ruotsala@gmail.com>
2014-05-22 07:19:51 -0700

    attempting strategy to solve

---
+++ 0079_passcode_derivation/passcode_derivation.py
@@ -0,0 +1,9 @@
+#Solution to Project Euler 79 by Ian Ruotsala
+
+PATH = "/home/ian/Documents/prog/projectEuler/0079_passcode_derivation/"
+FILE = "keylog.txt"
+
+def main():
+	string_set = set()
+	f = open(PATH + FILE, "r")
+main()
