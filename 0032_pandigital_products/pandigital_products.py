32b44cd63807611a46c3fc7a6bb3d8bd2baf9a01
Ian Ruotsala <ian.ruotsala@gmail.com>
2014-06-07 15:34:19 -0700

    making fn to check if pandigital

--- 0032_pandigital_products/pandigital_products.py
+++ 0032_pandigital_products/pandigital_products.py
@@ -1,3 +1,30 @@
+def is_pandigital(int_x, int_y, int_z):
+	digits = "123456789"
+	str_x = str(int_x)
+	str_y = str(int_y)
+	str_z = str(int_z)
+	for dig in digits:
+		if (str_x.find(dig) != -1):
+			str_x.replace(dig, "")
+		elif (str_y.find(dig) != -1):
+			str_y.replace(dig, "")
+		elif (str_z.find(dig) != -1):
+			str_z.replace(dig, "")
+		else:
+			return False
+
+	if (len(str_x) != 0 or len(str_y) != 0 or len(str_z) != 0):
+		return False
+
+	return True
+
+def test():
+	print "testing"
+	print "true: " + str(is_pandigital(123,456,789))
+	print "false: " + str(is_pandigital(12,-345,6789))
+	print "false: " + str(is_pandigital(1,2,3))
+
 def main():
-	is_pandigital(1,2,3)
+	test()
+
 main()
