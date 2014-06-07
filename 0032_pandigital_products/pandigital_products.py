79a2bf8595186cd3574b464ebf236131691590d8
Ian Ruotsala <ian.ruotsala@gmail.com>
2014-06-07 15:38:31 -0700

    fixing bug in is_pandigital fn

--- 0032_pandigital_products/pandigital_products.py
+++ 0032_pandigital_products/pandigital_products.py
@@ -5,14 +5,15 @@ def is_pandigital(int_x, int_y, int_z):
 	str_z = str(int_z)
 	for dig in digits:
 		if (str_x.find(dig) != -1):
-			str_x.replace(dig, "")
+			str_x = str_x.replace(dig, "")
 		elif (str_y.find(dig) != -1):
-			str_y.replace(dig, "")
+			str_y = str_y.replace(dig, "")
 		elif (str_z.find(dig) != -1):
-			str_z.replace(dig, "")
+			str_z = str_z.replace(dig, "")
 		else:
 			return False
 
+	print str_x + str_y + str_z
 	if (len(str_x) != 0 or len(str_y) != 0 or len(str_z) != 0):
 		return False
 
